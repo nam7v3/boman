9d3c144def37af28fc6d4689534de3493f43f23a
electrostrike <knguynvitanh@gmail.com>
2022-10-26 01:30:38 +0700

    Thêm sound

--- src/main/java/com/github/boman/util/SoundEffects.java
+++ src/main/java/com/github/boman/util/SoundEffects.java
@@ -1,14 +1,49 @@
 package com.github.boman.util;
 
-import java.io.File;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoundEffects {
-    public static File bgm = new File(Objects.requireNonNull(SoundEffects.class.getResource("bgm.wav")).getFile());
-    public static File bomberDied = new File(Objects.requireNonNull(SoundEffects.class.getResource("bomberDied.wav")).getFile());
-    public static File bombExploded = new File(Objects.requireNonNull(SoundEffects.class.getResource("bombExploded.wav")).getFile());
-    public static File bombPlanted = new File(Objects.requireNonNull(SoundEffects.class.getResource("bombPlanted.wav")).getFile());
-    public static File gameOver = new File(Objects.requireNonNull(SoundEffects.class.getResource("gameOver.wav")).getFile());
-    public static File itemPicked = new File(Objects.requireNonNull(SoundEffects.class.getResource("itemPicked.wav")).getFile());
-    public static File win = new File(Objects.requireNonNull(SoundEffects.class.getResource("win.wav")).getFile());
+    public enum SoundIndex {
+        BGM(0),
+        BOMBER_DIED(1),
+        BOMB_EXPLODED(2),
+        BOMB_PLANTED(3),
+        GAME_OVER(4),
+        ITEM_PICKED(5),
+        WIN(6),
+        MAIN_MENU(7);
+        private int value;
+
+        private SoundIndex(int value) {
+            this.value = value;
+        }
+    }
+    private List<Sound> sounds;
+    public static SoundEffects instance = new SoundEffects();
+    private SoundEffects() {
+        sounds = new ArrayList<>();
+        sounds.add(new Sound("bgm.wav", true));
+        sounds.add(new Sound("bomberDied.wav", false));
+        sounds.add(new Sound("bombExploded.wav", false));
+        sounds.add(new Sound("bombPlanted.wav", false));
+        sounds.add(new Sound("gameOver.wav", false));
+        sounds.add(new Sound("itemPicked.wav", false));
+        sounds.add(new Sound("win.wav", false));
+        sounds.add(new Sound("mainMenu.wav", true));
+    }
+
+    public void playSound(SoundIndex index) {
+        sounds.get(index.value).audioClip.play();
+    }
+
+    public void stopSound(SoundIndex index) {
+        sounds.get(index.value).audioClip.stop();
+    }
+
+    public void setVolumeSound(double volume) {
+        for (Sound sound: sounds) {
+            sound.audioClip.setVolume(volume);
+        }
+    }
 }
