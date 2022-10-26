cf0107176e147aa3c26c849986c2623d8895674a
nam7v3 <nam7v3@gmail.com>
2022-10-26 17:58:33 +0700

    Refactor lại sound.

--- src/main/java/com/github/boman/util/SoundEffects.java
+++ src/main/java/com/github/boman/util/SoundEffects.java
@@ -1,7 +1,12 @@
 package com.github.boman.util;
 
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SoundEffects {
     public enum SoundIndex {
@@ -19,31 +24,43 @@ public class SoundEffects {
             this.value = value;
         }
     }
-    private List<Sound> sounds;
+
+    private List<AudioClip> sounds;
     public static SoundEffects instance = new SoundEffects();
+
     private SoundEffects() {
         sounds = new ArrayList<>();
-        sounds.add(new Sound("bgm.wav", true));
-        sounds.add(new Sound("bomberDied.wav", false));
-        sounds.add(new Sound("bombExploded.wav", false));
-        sounds.add(new Sound("bombPlanted.wav", false));
-        sounds.add(new Sound("gameOver.wav", false));
-        sounds.add(new Sound("itemPicked.wav", false));
-        sounds.add(new Sound("win.wav", false));
-        sounds.add(new Sound("mainMenu.wav", true));
+        sounds.add(loadAudioClip("bgm.wav", true));
+        sounds.add(loadAudioClip("bomberDied.wav", false));
+        sounds.add(loadAudioClip("bombExploded.wav", false));
+        sounds.add(loadAudioClip("bombPlanted.wav", false));
+        sounds.add(loadAudioClip("gameOver.wav", false));
+        sounds.add(loadAudioClip("itemPicked.wav", false));
+        sounds.add(loadAudioClip("win.wav", false));
+        sounds.add(loadAudioClip("mainMenu.wav", true));
+    }
+
+    public AudioClip loadAudioClip(String filename, boolean infinite) {
+        File file = new File(Objects.requireNonNull(getClass().getResource(filename)).getFile());
+        Media media = new Media(file.toURI().toString());
+        AudioClip audioClip = new AudioClip(media.getSource());
+        if (infinite) {
+            audioClip.setCycleCount(AudioClip.INDEFINITE);
+        }
+        return audioClip;
     }
 
     public void playSound(SoundIndex index) {
-        sounds.get(index.value).audioClip.play();
+        sounds.get(index.value).play();
     }
 
     public void stopSound(SoundIndex index) {
-        sounds.get(index.value).audioClip.stop();
+        sounds.get(index.value).stop();
     }
 
     public void setVolumeSound(double volume) {
-        for (Sound sound: sounds) {
-            sound.audioClip.setVolume(volume);
+        for (AudioClip audioClip : sounds) {
+            audioClip.setVolume(volume);
         }
     }
 }
